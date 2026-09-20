e69b4ae2d173154c408fcdab1c28a13dfe76ea25
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-20 23:07:40 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,21 +1,54 @@
-
-
 public class User {
+    private int id;
     private String name;
     private String email;
     private String password;
-    public User(String name, String email, String password) {
+    private String role;
+
+    public User(int id, String name, String email, String password, String role) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
+
+    public int getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPassword() {
         return password;
     }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String toString() {
+        return id + " | " + name + " | " + email + " | " + role;
+    }
 }
+
+
+   
