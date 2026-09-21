976bee6f434473828934c9bdbc878a258025b274
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:41:47 +0530

    Update UserData.java

--- UserData.java
+++ UserData.java
@@ -4,67 +4,36 @@ public class UserData {
 
     public static ArrayList<User> users = new ArrayList<>();
 
-    private static int nextId = 1;
-
     static {
+
         users.add(new User(
-            nextId++,
-            "Owner",
-            "owner@indirahub.com",
-            "owner123",
-            "OWNER"
+                1,
+                "Owner",
+                "owner@indirahub.com",
+                "owner123",
+                "OWNER"
         ));
 
         users.add(new User(
-            nextId++,
-            "Seller",
-            "seller@indirahub.com",
-            "seller123",
-            "SELLER"
-        ));
-
-        users.add(new User(
-            nextId++,
-            "Customer",
-            "customer@indirahub.com",
-            "customer123",
-            "CUSTOMER"
+                2,
+                "Seller",
+                "seller@indirahub.com",
+                "seller123",
+                "SELLER"
         ));
     }
 
-    public static User register(
-            String name,
-            String email,
-            String password,
-            String role) {
-
-        User user = new User(
-            nextId++,
-            name,
-            email,
-            password,
-            role
-        );
-
+    public static void addUser(User user) {
         users.add(user);
-
-        return user;
-    }
-
-    public static User findByEmail(String email) {
-        for (User user : users) {
-            if (user.getEmail().equalsIgnoreCase(email)) {
-                return user;
-            }
-        }
-
-        return null;
     }
 
     public static User login(String email, String password) {
+
         for (User user : users) {
-            if (user.getEmail().equalsIgnoreCase(email)
+
+            if (user.getEmail().equals(email)
                     && user.getPassword().equals(password)) {
+
                 return user;
             }
         }
