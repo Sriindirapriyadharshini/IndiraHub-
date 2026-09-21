7d0aa8df23d19495cbf72394cc435d5a250fe660
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:42:31 +0530

    Update OrderData.java

--- OrderData.java
+++ OrderData.java
@@ -4,35 +4,11 @@ public class OrderData {
 
     public static ArrayList<Order> orders = new ArrayList<>();
 
-    private static int nextOrderId = 1;
-
-    public static Order createOrder(
-            int customerId,
-            ArrayList<OrderItem> items,
-            double total) {
-
-        Order order = new Order(
-            nextOrderId++,
-            customerId,
-            items,
-            total
-        );
-
+    public static void addOrder(Order order) {
         orders.add(order);
-
-        return order;
     }
 
-    public static ArrayList<Order> getCustomerOrders(int customerId) {
-
-        ArrayList<Order> result = new ArrayList<>();
-
-        for (Order order : orders) {
-            if (order.getCustomerId() == customerId) {
-                result.add(order);
-            }
-        }
-
-        return result;
+    public static ArrayList<Order> getOrders() {
+        return orders;
     }
 }
