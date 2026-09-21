3e4fa057fa4bae0c480a576071b416abc9061c55
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:39:19 +0530

    Refactor User class constructor and remove methods
    Refactor User constructor for better readability and remove unused setter methods and toString method.

--- User.java
+++ User.java
@@ -1,11 +1,14 @@
 public class User {
+
     private int id;
     private String name;
     private String email;
     private String password;
     private String role;
 
-    public User(int id, String name, String email, String password, String role) {
+    public User(int id, String name, String email,
+                String password, String role) {
+
         this.id = id;
         this.name = name;
         this.email = email;
@@ -32,23 +35,4 @@ public class User {
     public String getRole() {
         return role;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String toString() {
-        return id + " | " + name + " | " + email + " | " + role;
-    }
 }
-
-
-   
