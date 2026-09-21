bd50ad25556d6cc77bc51236133d9972f694bb66
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:42:53 +0530

    Refactor login method to accept parameters

--- Login.java
+++ Login.java
@@ -1,29 +1,7 @@
-import java.util.Scanner;
-
 public class Login {
 
-    public static User login() {
+    public static User loginUser(String email, String password) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("\n===== INDIRAHUB LOGIN =====");
-
-        System.out.print("Email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Password: ");
-        String password = sc.nextLine();
-
-        User user = UserData.login(email, password);
-
-        if (user == null) {
-            System.out.println("Invalid email or password.");
-            return null;
-        }
-
-        System.out.println("Login successful.");
-        System.out.println("Welcome " + user.getName());
-
-        return user;
+        return UserData.login(email, password);
     }
 }
