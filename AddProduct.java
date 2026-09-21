b34a9c55e03d1cdce79f48deae475d70f63336ac
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:43:43 +0530

    Refactor addProduct method to use parameters

--- AddProduct.java
+++ AddProduct.java
@@ -1,34 +1,24 @@
-import java.util.Scanner;
-
 public class AddProduct {
 
-    public static void addProduct(User seller) {
+    public static void add(String name,
+                           String author,
+                           double price,
+                           int quantity,
+                           String category,
+                           String image) {
 
-        Scanner sc = new Scanner(System.in);
+        int id = ProductData.products.size() + 1;
 
-        System.out.println("\n===== ADD PRODUCT =====");
-
-        System.out.print("Product name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Category: ");
-        String category = sc.nextLine();
-
-        System.out.print("Price: ");
-        double price = sc.nextDouble();
-
-        System.out.print("Quantity: ");
-        int quantity = sc.nextInt();
-
-        Product product = ProductData.addProduct(
-            name,
-            category,
-            price,
-            quantity,
-            seller.getId()
+        Product product = new Product(
+                id,
+                name,
+                author,
+                price,
+                quantity,
+                category,
+                image
         );
 
-        System.out.println("Product added successfully.");
-        System.out.println(product);
+        ProductData.addProduct(product);
     }
 }
