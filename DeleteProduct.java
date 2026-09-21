c5421ae44abbc2aeee3d5660df519054c28bf8c1
Sriindirapriyadharshini <indiraganesan1866@gmail.com>
2026-09-21 10:45:04 +0530

    Refactor deleteProduct method to accept ID parameter
    Refactored deleteProduct method to delete by product ID directly.

--- DeleteProduct.java
+++ DeleteProduct.java
@@ -1,28 +1,7 @@
-import java.util.Scanner;
-
 public class DeleteProduct {
 
-    public static void deleteProduct(User seller) {
+    public static void delete(int id) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter product ID: ");
-        int id = sc.nextInt();
-
-        Product product = ProductData.findById(id);
-
-        if (product == null) {
-            System.out.println("Product not found.");
-            return;
-        }
-
-        if (product.getSellerId() != seller.getId()) {
-            System.out.println("You cannot delete this product.");
-            return;
-        }
-
-        ProductData.removeProduct(id);
-
-        System.out.println("Product deleted successfully.");
+        ProductData.deleteProduct(id);
     }
 }
